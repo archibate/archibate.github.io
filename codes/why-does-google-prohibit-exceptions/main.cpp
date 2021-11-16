0969ac7241411af3dfe26c0ecb7946fdb6785766
archibate <1931127624@qq.com>
2021-11-16 09:38:07 +0800

    this is why

---
+++ codes/why-does-google-prohibit-exceptions/main.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <cstdio>
+
+
+struct A {
+    A() noexcept {
+        printf("%p: A()\n", this);
+    }
+
+    A(A &&that) noexcept {
+        printf("%p <- %p: A(A &&)\n", this, &that);
+    }
+
+    A(A const &that) noexcept {
+        printf("%p <- %p: A(A const &)\n", this, &that);
+    }
+
+    A &operator=(A &&that) noexcept {
+        printf("%p <- %p: A &operator=(A &&)\n", this, &that);
+        return *this;
+    }
+
+    A &operator=(A const &that) noexcept {
+        printf("%p <- %p: A &operator=(A const &)\n", this, &that);
+        return *this;
+    }
+
+    ~A() noexcept {
+        printf("%p: ~A()\n", this);
+    }
+};
+
+
+int main() {
+    std::vector<A> a(2);
+    a.resize(3);
+    return 0;
+}
